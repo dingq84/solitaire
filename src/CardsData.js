4df71ad0f8e1afac2597db14ef7e429c240a88f6
Dean <dingq84@gmail.com>
2019-07-21 00:50:26 +0800

    add svg

--- src/CardsData.js
+++ src/CardsData.js
@@ -1,10 +1,25 @@
-export default function CardsData(){
+import React from 'react';
+
+export default function CardsData(dragStart, drop, cancelDefault){
   const suits = ['spade', 'heart', 'diamond', 'club'];
-  const cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const cards = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
   let totalCards = [];
   for(let i = 0; i < suits.length; i++){
     for (let j = 0; j < cards.length; j++){
-      totalCards.push(suits[i] + '-' + cards[j])
+      totalCards.push(
+        <img
+          key={suits[i] + cards[j]}
+          id={suits[i] + cards[j]}
+          src={require(`./assets/images/card-${suits[i]}-${cards[j]}.svg`)}
+          alt={suits[i] + cards[j]}
+          onDragStart={(e) => dragStart(e)}
+          onDragOver={(e) => cancelDefault(e)}
+          onDragEnter={(e) => cancelDefault(e)}
+          onDrop={(e) => drop(e)}
+          draggable="true"
+        >
+        </img>
+      )
     }
   }
 
@@ -17,47 +32,38 @@ export default function CardsData(){
   }
 
   const split = totalCards => {
-      const shuffledArray = shuffle(totalCards);
-      let one = [];
-      let two = [];
-      let three = [];
-      let four = [];
-      let five = [];
-      let six = [];
-      let seven = [];
-      let eight = [];
-      for(let i = 0; i <shuffledArray.length; i++){
-        switch (i % 8) {
-          case 1:
-            one.push(shuffledArray[i]);
-            break;
-          case 2:
-            two.push(shuffledArray[i]);
-            break;
-          case 3:
-            three.push(shuffledArray[i]);
-            break;
-          case 4:
-            four.push(shuffledArray[i]);
-            break;
-          case 5:
-            five.push(shuffledArray[i]);
-            break;
-          case 6:
-            six.push(shuffledArray[i]);
-            break;
-          case 7:
-            seven.push(shuffledArray[i]);
-            break;
-          default:
-            eight.push(shuffledArray[i]);
-
+    const arr = [[], [], [] ,[], [], [], [], []];
+    const shuffledArray = shuffle(totalCards);
+    for(let i = 0; i <shuffledArray.length; i++){
+      switch (i % 8) {
+        case 1:
+          arr[0].push(shuffledArray[i]);
+          break;
+        case 2:
+          arr[1].push(shuffledArray[i]);
+          break;
+        case 3:
+          arr[2].push(shuffledArray[i]);
+          break;
+        case 4:
+          arr[3].push(shuffledArray[i]);
+          break;
+        case 5:
+          arr[4].push(shuffledArray[i]);
+          break;
+        case 6:
+          arr[5].push(shuffledArray[i]);
+          break;
+        case 7:
+          arr[6].push(shuffledArray[i]);
+          break;
+        default:
+          arr[7].push(shuffledArray[i]);
         }
-      }
-      return [one, two, three, four, five, six ,seven, eight]
+    }
+
+    return arr;
   }
-
-
 
   return split(totalCards);
 }
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from 'react';
 
 import CardsData from './CardsData';
-import {CardsModel} from './CardsModel';
+// import {CardsModel} from './CardsModel';
+
+import club from './assets/images/club-24px.svg';
+import diamond from './assets/images/diamond-24px.svg';
+import heart from './assets/images/heart-24px.svg';
+import spade from './assets/images/spade-24px.svg';
 
 import './_Main.scss';
+
 export default function Main(){
-  const [one, two, three, four, five, six ,seven, eight] = CardsData();
+  const [one, two, three, four, five, six ,seven, eight] = CardsData(dragStart, drop, cancelDefault);
 
   function dragStart(e){
     console.log(e.target.id);
@@ -17,11 +23,11 @@ export default function Main(){
     cancelDefault(e)
     let id = e.dataTransfer.getData('text/plain');
     const target = document.getElementById(id);
-    console.log(e.target.style.top !== '' && e.target.style.top.replace('%', ''))
-    
-    target.style.top = e.target.style.top ? '10%' : '0%';
-    target.style.left = '0';
-    target.style.zIndex = parseInt(e.target.style.zIndex) + 1 || '0';
+    console.log(e.target)
+
+    // target.style.top = e.target.style.top ? '10%' : '0%';
+    // target.style.left = '0';
+    // target.style.zIndex = parseInt(e.target.style.zIndex) + 1 || '0';
     e.target.appendChild(target);
   }
 
@@ -33,53 +39,90 @@ export default function Main(){
   }
   return (
     <div className="solitaire__wrap">
+      <div className="solitaire__wrap__sidebar">
+        <div className="solitaire__wrap__sidebar__flag">
+          <span>FREE</span>
+          <span>CELL</span>
+        </div>
+        <span className="solitaire__wrap__sidebar__time">
+          00:30
+        </span>
+        <div className="solitaire__wrap__sidebar__buttons">
+          <button>UNDO</button>
+          <button>PAUSE</button>
+          <button>RESTART</button>
+        </div>
+      </div>
       <div className="solitaire__wrap__place">
         <div className="solitaire__wrap__place__left">
-          <div 
+          <div
             className="solitaire__wrap__place__left__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
-          ></div>
-          <div 
+          >
+            <img
+              src={spade}
+              alt='spade'
+            ></img>
+          </div>
+          <div
             className="solitaire__wrap__place__left__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
-          ></div>
-          <div 
+          >
+            <img
+              src={heart}
+              alt='heart'
+            >
+            </img>
+          </div>
+          <div
             className="solitaire__wrap__place__left__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
-          ></div>
-          <div 
+          >
+            <img
+              src={club}
+              alt='club'
+            >
+            </img>
+          </div>
+          <div
             className="solitaire__wrap__place__left__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
-          ></div>
+          >
+            <img
+              src={diamond}
+              alt='diamond'
+            >
+            </img>
+          </div>
         </div>
         <div className="solitaire__wrap__place__right">
-          <div 
+          <div
             className="solitaire__wrap__place__right__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
           ></div>
-          <div 
+          <div
             className="solitaire__wrap__place__right__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
           ></div>
-          <div 
+          <div
             className="solitaire__wrap__place__right__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
             onDrop={(e) => drop(e)}
           ></div>
-          <div 
+          <div
             className="solitaire__wrap__place__right__items"
             onDragOver={(e) => cancelDefault(e)}
             onDragEnter={(e) => cancelDefault(e)}
@@ -88,166 +131,62 @@ export default function Main(){
         </div>
       </div>
       <div className="solitaire__wrap__cards">
-        <div 
+        <div
           className="solitaire__wrap__cards__first"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            one.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {one.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__second"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            two.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {two.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__third"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            three.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {three.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__forth"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            four.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {four.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__fifth"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            five.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {five.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__sixth"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            six.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {six.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__seventh"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            seven.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {seven.map(item => item)}
         </div>
         <div className="solitaire__wrap__cards__eighth"
           onDragOver={(e) => cancelDefault(e)}
           onDragEnter={(e) => cancelDefault(e)}
           onDrop={(e) => drop(e)}
         >
-          {
-            eight.map((item, index) =>
-              <CardsModel
-                key={index}
-                data={item}
-                top={`${index * 10}%`}
-                zIndex = {index}
-                dragStart={dragStart}
-                dragOver={cancelDefault}
-                dragEnter={cancelDefault}
-                drop={drop}
-              />
-            )
-          }
+          {eight.map(item => item)}
         </div>
       </div>
     </div>
