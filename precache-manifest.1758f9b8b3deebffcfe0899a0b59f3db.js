e9d987bfeda42e1fe3b82ed50e2830ede62a81cd
dingq84 <dingq84@gmail.com>
2019-07-27 16:40:45 +0800

    Updates

---
+++ precache-manifest.1758f9b8b3deebffcfe0899a0b59f3db.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b0f946e85d0bd937b0a12341b9f30b",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "4cbfc891679d6ecd50fa",
+    "url": "/solitaire/static/css/main.f708a511.chunk.css"
+  },
+  {
+    "revision": "9d3a48c745359953aab9",
+    "url": "/solitaire/static/js/2.7443c68a.chunk.js"
+  },
+  {
+    "revision": "4cbfc891679d6ecd50fa",
+    "url": "/solitaire/static/js/main.219e8adf.chunk.js"
+  },
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "400ad2a2a96e3b9da2288fb42bb81399",
+    "url": "/solitaire/static/media/card-club-1.400ad2a2.svg"
+  },
+  {
+    "revision": "289b0595d182e0f5303dee80989558bd",
+    "url": "/solitaire/static/media/card-club-10.289b0595.svg"
+  },
+  {
+    "revision": "9048ec619c2abfec550744fe219e51c5",
+    "url": "/solitaire/static/media/card-club-11.9048ec61.svg"
+  },
+  {
+    "revision": "78d3aa29ad657c68db54c1e5639f0255",
+    "url": "/solitaire/static/media/card-club-12.78d3aa29.svg"
+  },
+  {
+    "revision": "bc58af54d9cb7105235ee82e39455b2a",
+    "url": "/solitaire/static/media/card-club-13.bc58af54.svg"
+  },
+  {
+    "revision": "975ace6b8fac280ce88be438b3154a4a",
+    "url": "/solitaire/static/media/card-club-2.975ace6b.svg"
+  },
+  {
+    "revision": "51f5f7bab8789ee0dfe39512017380c7",
+    "url": "/solitaire/static/media/card-club-3.51f5f7ba.svg"
+  },
+  {
+    "revision": "15c24ac7adfce5e4b8ebb8684390cecb",
+    "url": "/solitaire/static/media/card-club-4.15c24ac7.svg"
+  },
+  {
+    "revision": "004cf2fb8d916a4208a3afaded1b6353",
+    "url": "/solitaire/static/media/card-club-5.004cf2fb.svg"
+  },
+  {
+    "revision": "e928144e59281d7abd49a99e6a765ffc",
+    "url": "/solitaire/static/media/card-club-6.e928144e.svg"
+  },
+  {
+    "revision": "53ca1f7d05cf9f1d13b257e0a27bc320",
+    "url": "/solitaire/static/media/card-club-7.53ca1f7d.svg"
+  },
+  {
+    "revision": "ddcc913e80377eed20be6401868a56df",
+    "url": "/solitaire/static/media/card-club-8.ddcc913e.svg"
+  },
+  {
+    "revision": "2f97ffab54442c65eec6bc229ab81fe9",
+    "url": "/solitaire/static/media/card-club-9.2f97ffab.svg"
+  },
+  {
+    "revision": "97ec09d8520a82ef7f29a3176c12fccd",
+    "url": "/solitaire/static/media/card-diamond-1.97ec09d8.svg"
+  },
+  {
+    "revision": "ae34b8469aa4915d6eb7482d7fa1859f",
+    "url": "/solitaire/static/media/card-diamond-10.ae34b846.svg"
+  },
+  {
+    "revision": "3ff5f16b91de9e500704bb7b8fcb53da",
+    "url": "/solitaire/static/media/card-diamond-11.3ff5f16b.svg"
+  },
+  {
+    "revision": "c9c48e3e619ebdd8cbb991bc2aa47c9e",
+    "url": "/solitaire/static/media/card-diamond-12.c9c48e3e.svg"
+  },
+  {
+    "revision": "9406f24efcaf4410ff2cd4ed992448b1",
+    "url": "/solitaire/static/media/card-diamond-13.9406f24e.svg"
+  },
+  {
+    "revision": "e8de0162cd5841eb4820b574f0678599",
+    "url": "/solitaire/static/media/card-diamond-2.e8de0162.svg"
+  },
+  {
+    "revision": "87ef9e1395951e3695385955c15fd089",
+    "url": "/solitaire/static/media/card-diamond-3.87ef9e13.svg"
+  },
+  {
+    "revision": "506df3c6e7599f8db9f89410167b0ab7",
+    "url": "/solitaire/static/media/card-diamond-4.506df3c6.svg"
+  },
+  {
+    "revision": "71ee6635a39b10d0cf95a9c6af3a48ff",
+    "url": "/solitaire/static/media/card-diamond-5.71ee6635.svg"
+  },
+  {
+    "revision": "6f59a9fca19b57d77e7417808d97d030",
+    "url": "/solitaire/static/media/card-diamond-6.6f59a9fc.svg"
+  },
+  {
+    "revision": "129d60bc6e5e20b41384add85cbe12cb",
+    "url": "/solitaire/static/media/card-diamond-7.129d60bc.svg"
+  },
+  {
+    "revision": "7ecf3ef71758bf598349d192e1ed96a0",
+    "url": "/solitaire/static/media/card-diamond-8.7ecf3ef7.svg"
+  },
+  {
+    "revision": "cfc34fce37b295fea25d0831fd1667c2",
+    "url": "/solitaire/static/media/card-diamond-9.cfc34fce.svg"
+  },
+  {
+    "revision": "80db9eedccbea722e834a707c8e21c7e",
+    "url": "/solitaire/static/media/card-heart-1.80db9eed.svg"
+  },
+  {
+    "revision": "66ba4937e594c12dac4ef5131f7857ec",
+    "url": "/solitaire/static/media/card-heart-10.66ba4937.svg"
+  },
+  {
+    "revision": "d8d81aa39052d36d8a00c630aeab1f6b",
+    "url": "/solitaire/static/media/card-heart-11.d8d81aa3.svg"
+  },
+  {
+    "revision": "54773fe754228f50c518eeda71c511cb",
+    "url": "/solitaire/static/media/card-heart-12.54773fe7.svg"
+  },
+  {
+    "revision": "bacb9544806b61c29d7eead007ed90ce",
+    "url": "/solitaire/static/media/card-heart-13.bacb9544.svg"
+  },
+  {
+    "revision": "4c4e2b1ca4684d85923f8a80d517a990",
+    "url": "/solitaire/static/media/card-heart-2.4c4e2b1c.svg"
+  },
+  {
+    "revision": "59a74feab181c74687b0a91a474f3d41",
+    "url": "/solitaire/static/media/card-heart-3.59a74fea.svg"
+  },
+  {
+    "revision": "f62df5d4ce744c87d7dd78d45ea6504f",
+    "url": "/solitaire/static/media/card-heart-4.f62df5d4.svg"
+  },
+  {
+    "revision": "ddd959b2343bc8e972b5fd24b7c97601",
+    "url": "/solitaire/static/media/card-heart-5.ddd959b2.svg"
+  },
+  {
+    "revision": "7bb5af28c24e1d63989f1769fca90803",
+    "url": "/solitaire/static/media/card-heart-6.7bb5af28.svg"
+  },
+  {
+    "revision": "218f6f59bb45da2a93691d92014e5988",
+    "url": "/solitaire/static/media/card-heart-7.218f6f59.svg"
+  },
+  {
+    "revision": "4898e40a2aee8d8d8401de8af6265171",
+    "url": "/solitaire/static/media/card-heart-8.4898e40a.svg"
+  },
+  {
+    "revision": "fa39481da7254f4d88dd6d44d0e1d6df",
+    "url": "/solitaire/static/media/card-heart-9.fa39481d.svg"
+  },
+  {
+    "revision": "98dccc32ad9a9c2fe0e3311381ac7665",
+    "url": "/solitaire/static/media/card-spade-1.98dccc32.svg"
+  },
+  {
+    "revision": "eb0d6568d0c6bf812f5af7c1c7b96a73",
+    "url": "/solitaire/static/media/card-spade-10.eb0d6568.svg"
+  },
+  {
+    "revision": "4ff9e362004238e30d4214a5d58f2dd4",
+    "url": "/solitaire/static/media/card-spade-11.4ff9e362.svg"
+  },
+  {
+    "revision": "f542c3a035696d680a6a270b6282ec73",
+    "url": "/solitaire/static/media/card-spade-12.f542c3a0.svg"
+  },
+  {
+    "revision": "8995fbd474d5a41cd4d61de3011affd0",
+    "url": "/solitaire/static/media/card-spade-13.8995fbd4.svg"
+  },
+  {
+    "revision": "3bd0db0a059375ffccdf941f36102471",
+    "url": "/solitaire/static/media/card-spade-2.3bd0db0a.svg"
+  },
+  {
+    "revision": "c2cdcc8f0c61bbfd44035b4221e5fdf9",
+    "url": "/solitaire/static/media/card-spade-3.c2cdcc8f.svg"
+  },
+  {
+    "revision": "1440d878a5485a6aba0440523f4f8923",
+    "url": "/solitaire/static/media/card-spade-4.1440d878.svg"
+  },
+  {
+    "revision": "e4fcf9998b0220d362ccd1702e6fdf06",
+    "url": "/solitaire/static/media/card-spade-5.e4fcf999.svg"
+  },
+  {
+    "revision": "2a8d55cbb322646691a12578fcf25775",
+    "url": "/solitaire/static/media/card-spade-6.2a8d55cb.svg"
+  },
+  {
+    "revision": "b097a7f4f827d4b75b6714a8cd909175",
+    "url": "/solitaire/static/media/card-spade-7.b097a7f4.svg"
+  },
+  {
+    "revision": "f3968babc353d4e9199a58cbe4794dd3",
+    "url": "/solitaire/static/media/card-spade-8.f3968bab.svg"
+  },
+  {
+    "revision": "c28b6442b04c839f8fed88384b7dd115",
+    "url": "/solitaire/static/media/card-spade-9.c28b6442.svg"
+  },
+  {
+    "revision": "a3fd5d4072b87707a7574eda9ff31adc",
+    "url": "/solitaire/static/media/club-24px.a3fd5d40.svg"
+  },
+  {
+    "revision": "c1a4529f5822c6e56724b69df4c7f364",
+    "url": "/solitaire/static/media/diamond-24px.c1a4529f.svg"
+  },
+  {
+    "revision": "b402beaa0f0574d9997e8b593d590b27",
+    "url": "/solitaire/static/media/heart-24px.b402beaa.svg"
+  },
+  {
+    "revision": "6d01e1cbc7db877db4f2782d172aa62d",
+    "url": "/solitaire/static/media/ic_loop_24px.6d01e1cb.svg"
+  },
+  {
+    "revision": "33c55e2f76dfe03e76f2218867527e0a",
+    "url": "/solitaire/static/media/ic_pause_24px.33c55e2f.svg"
+  },
+  {
+    "revision": "456e46970baf84fe83f4012d2f5e84d2",
+    "url": "/solitaire/static/media/ic_play_24px.456e4697.svg"
+  },
+  {
+    "revision": "16b81a0f0f3471e549d82bab8ca233e0",
+    "url": "/solitaire/static/media/ic_undo_24px.16b81a0f.svg"
+  },
+  {
+    "revision": "231117825bfc9593d973c9b67fc7d7d7",
+    "url": "/solitaire/static/media/spade-24px.23111782.svg"
+  }
+]);
